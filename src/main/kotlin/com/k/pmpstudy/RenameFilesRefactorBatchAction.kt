ccbb4c45914688e4f9d7e49930b84add738d2df6
Miura Kotaro <miura-c0nt@k-pmpstudy.com>
2021-08-14 17:46:09 +0900

    フォルダ内のファイルの一括リネームができるようになった。

--- src/main/kotlin/com/k/pmpstudy/RenameFilesRefactorBatchAction.kt
+++ src/main/kotlin/com/k/pmpstudy/RenameFilesRefactorBatchAction.kt
@@ -4,15 +4,19 @@ import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.*
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiClassOwner
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFileSystemItem
 import com.intellij.refactoring.RefactoringFactory
 
 class RenameFilesRefactorBatchAction : AnAction() {
+    private lateinit var refactoringFactory: RefactoringFactory
+
     override fun update(e: AnActionEvent) {
-        e.presentation.isEnabledAndVisible = (e.project != null) && isOnDirectoryInProjectView(e)
+        e.presentation.isEnabledAndVisible = isOnDirectoryInProjectView(e)
     }
 
     private fun isOnDirectoryInProjectView(e: AnActionEvent): Boolean {
@@ -25,23 +29,28 @@ class RenameFilesRefactorBatchAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project
+        refactoringFactory = RefactoringFactory.getInstance(project)
 
-        val selectedPsiElement: PsiElement = e.getData(CommonDataKeys.PSI_ELEMENT)!!
-        val child: PsiElement = selectedPsiElement.children[0]
-        if (child is PsiJavaFile) {
-            if (project != null) {
-                renameFilesRefactor(project, child.classes[0])
-            }
-        }
+        val directoryPsi: PsiElement = e.getData(CommonDataKeys.PSI_ELEMENT)!!
+        renameFilesRefactor(directoryPsi)
     }
 
     private fun inputReplaceWord(): Map<String, String> {
         return mapOf("search" to "word1", "replace" to "word2")
     }
 
-    private fun renameFilesRefactor(project: Project, psiClass: PsiClass) {
-        val renameRefactoring = RefactoringFactory.getInstance(project).createRename(psiClass, "newName")
-        renameRefactoring.run()
+    private fun renameFilesRefactor(pathPsi: PsiElement) {
+        if (pathPsi !is PsiFileSystemItem) return
+        if (pathPsi.isDirectory) {
+            pathPsi.children.forEach { renameFilesRefactor(it) }
+        }
+        if (pathPsi is PsiClassOwner) {
+            val classes: Array<PsiClass> = pathPsi.classes
+            if (classes.size == 1) {
+                val name: String = classes[0].name.toString()
+                refactoringFactory.createRename(classes[0], name + "new").run()
+            }
+        }
     }
 
     private fun showTestMessage(test: String = "Test") {
